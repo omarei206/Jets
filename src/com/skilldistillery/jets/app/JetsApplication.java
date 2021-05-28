3b395866e34364a1fbffa24c390b88bea32421bb
Omarei <omareigregory@gmail.com>
2021-05-28 15:23:25 -0600

    initial commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entity.Airfield;
+
+public class JetsApplication {
+
+	public static void main(String[] args) {
+		
+		Airfield airfield;
+	}
+
+}
---
+++ src/com/skilldistillery/jets/entity/Airfield.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.jets.entity;
+
+import java.util.List;
+
+public class Airfield {
+	private List<Jet> jets;
+}
